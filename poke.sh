402ec3260e51caf201eac772f2c2ed1a41d1c6c5
schuylermartin45 <schuylermartin45@gmail.com>
2015-01-28 19:22:05 -0500

    Initial commit

---
+++ poke.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#Work through the sym links back to the script's actual running directory 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "${SOURCE}" ]; do 
+    DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
+    SOURCE="$(readlink "${SOURCE}")"
+    [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}"
+done
+declare -r DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
+
+#Check if all arguments are passed
+if [[ ${#} -lt 2 ]]; then
+    echo "Usage: poke.sh url [emails...]"
+    exit 1
+fi
+
+#Grab the url and shift the arguments
+declare -r URL="${1}"
+shift 1
+
+#Make diff directory, if need be
+declare -r POKEDIR="${DIR}/.pokediffs"
+declare -r DIFF_EXT=".diff"
+declare -r TEMP_EXT=".tmp"
+
+if [[ ! -d "${POKEDIR}" ]]; then
+    mkdir "${POKEDIR}"
+fi
+
+#Generate the unique name of the the diff file
+declare -r HASH_FILE="$(echo "$URL" | sha1sum)"
+declare -r DIFF_FILE="${HASH_FILE}${DIFF_EXT}"
+declare -r TEMP_FILE="${HASH_FILE}${TEMP_EXT}"
+
+curl -s "${URL}" > "${POKEDIR}/${TEMP_FILE}"
+
+if [[ -f "${POKEDIR}/${DIFF_FILE}" ]]; then
+    testStr=$(diff "${POKEDIR}/${TEMP_FILE}" "${POKEDIR}/${DIFF_FILE}")
+
+    #If the test string is not empty, there are differences. Alert the user(s) and
+    #and move the temp file over
+    if [[ ! -z ${testStr} ]]; then
+        mail -s "Update from ${URL}" "${@}" < "Update detected from this site.\n EOF"
+        echo "Email sent"
+        mv "${POKEDIR}/${TEMP_FILE}" "${POKEDIR}/${DIFF_FILE}"
+    else
+        rm "${POKEDIR}/${TEMP_FILE}"
+    fi
+else
+    mv "${POKEDIR}/${TEMP_FILE}" "${POKEDIR}/${DIFF_FILE}"
+    echo "No file to diff."
+fi
